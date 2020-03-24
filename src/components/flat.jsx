25835cb0601f73b273ed675fa0e451c1ffca4753
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-24 11:45:51 +0900

    added google maps react

--- src/components/flat.jsx
+++ src/components/flat.jsx
@@ -5,12 +5,12 @@ class Flat extends Component {
 
   render() {
 
-    const Background = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';
+    const imageUrl = 'https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat3.jpg';
 
     return (
       <Fragment>
         <div class="card" style={{
-          backgroundImage: `url(${Background})`
+          backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.3), rgba(0, 0, 0, 0.2)), url(${imageUrl})`
         }}>
           <div class="card-category">150 EUR</div>
           <div class="card-description">
