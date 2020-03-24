64f74496786a46df27cff8ba25751a94b77e921b
Nicholas Skibiak <nicholas.skibiak@gmail.com>
2020-03-24 11:03:39 +0900

    added holder divs

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+// import React from 'react';
 
-function App() {
-  return <p>"Help"</p>;
+// function App() {
+//   return <p>"Help"</p>;
+// }
+
+// export default App;
+
+import React, { Component } from 'react';
+// import Flat from './flat';
+
+class App extends Component {
+
+  render() {
+
+    return (
+      <div>
+        <div className='flat-list'>fart</div>
+        <div className='map-container'>another fart</div>
+      </div>
+    );
+  }
+
 }
 
 export default App;
